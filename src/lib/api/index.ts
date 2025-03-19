3203860554cc714238d62412075cf2c4b010c09e
SonHoangApusCorp <son.hoangvan@apuscorp.com>
2025-03-19 15:01:14 +0700

    first commit

---
+++ src/lib/api/index.ts
@@ -0,0 +1,25 @@
+export type Err = {
+  status?: number
+  message?: string
+  result?: string
+  errorMessages?: string[]
+  redirectUrl?: string
+  validationErrors?: {
+    title: string
+    errors: { name: string; messages: string[] }[]
+  }
+}
+
+export type ErrResponse = {
+  status: number
+  data: null
+  err: Err
+}
+
+export type DataResponse<T> = {
+  status: number
+  err: null
+  data: T
+}
+
+export type HttpResponse<T> = DataResponse<T> | ErrResponse
---
+++ src/pages/403.page.tsx
@@ -0,0 +1,14 @@
+// import { NoneLayout } from "@/components/layouts/WrapLayout/NoneLayout";
+import { HttpResponse } from "@/lib/api";
+import Page403 from "@/components/templates/common/403";
+import { NextPageWithLayout } from "@/lib/next/types";
+import { Meta } from "@/meta";
+
+type Props = HttpResponse<any>;
+
+const Page: NextPageWithLayout<Props> = () => <Page403 />;
+
+Page.getLayout = (page) => <>{page}</>;
+Page.getMeta = Meta(() => ({ title: "403" }));
+
+export default Page;
