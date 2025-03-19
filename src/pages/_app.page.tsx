cb006399516ded2526f75d15f5d6e7b780bbe6ad
SonHoangApusCorp <son.hoangvan@apuscorp.com>
2025-03-20 01:08:16 +0700

    fix

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,22 +1,23 @@
 import { AppPropsWithLayout } from "@/lib/next/types";
 import "../../public/styles/globals.css";
 import { Provider } from "react-redux";
-// import { store } from "@/redux/store";
+import { store } from "@/redux/store";
 import { persistStore } from "redux-persist";
 import { PersistGate } from "redux-persist/integration/react";
+
+let persistor = persistStore(store);
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page);
   const getMeta = Component.getMeta ?? ((page) => page);
-  // let persistor = persistStore(store);
 
   return getLayout(
     getMeta(
-      // <Provider store={store}>
-      //   <PersistGate loading={null} persistor={persistor}>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
           <Component {...pageProps} />,
-      //   </PersistGate>
-      // </Provider>,
+        </PersistGate>
+      </Provider>,
       pageProps
     )
   );
--- src/pages/dashboard/index.page.tsx
+++ src/pages/dashboard/index.page.tsx
@@ -3,6 +3,7 @@ import Dashboard from "@/components/templates/Dashboard";
 import { BasicLayout } from "@/layouts/BasicLayout";
 import { HttpResponse } from "@/lib/api";
 import { NextPageWithLayout } from "@/lib/next/types";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
 type Props = HttpResponse<any>;
 
@@ -11,4 +12,13 @@ const Page: NextPageWithLayout<Props> = () => <Dashboard />;
 Page.getLayout = BasicLayout;
 Page.getMeta = Meta(() => ({ title: "Dashboard Company Service" }));
 
+export const getServerSideProps = async (context: any) => {
+  const { locale = "vn" } = context;
+
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["common"])),
+    },
+  };
+};
 export default Page;
